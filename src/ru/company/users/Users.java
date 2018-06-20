b7ee8d687b9000f321b201ae9c69c092e70002f9
Egor <4156464@gmail.com>
2018-06-20 10:10:15 +0300

    Initial commit

---
+++ src/ru/company/users/Users.java
@@ -0,0 +1,13 @@
+package ru.company.users;
+
+public class Users {
+
+    private String name;
+    private String address;
+    private long tel;
+    private int age;
+    private String e_mail;
+
+
+}
+
